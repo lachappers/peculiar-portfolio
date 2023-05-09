e3bbc6cca3afaaa043d721a880792c2d9654181c
lachappers <116145040+lachappers@users.noreply.github.com>
2023-05-09 12:57:36 +0100

    cv style and responsiveness

--- js/script.js
+++ js/script.js
@@ -1,6 +1,25 @@
-const hamburger = document.querySelector(".hamburger");
-const navigation = document.querySelector(".navigation");
+// const hamburger = document.querySelector(".hamburger");
+// const navigation = document.querySelector(".navigation");
 
-hamburger.addEventListener("click", function () {
-  navigation.classList.toggle("open");
-});
+// hamburger.addEventListener("click", function () {
+//   navigation.classList.toggle("open");
+// });
+
+const expandable = document.getElementsByClassName("read-more");
+
+expandable.addEventListener("click", expand());
+
+function expand() {
+  var read = document.getElementsByClassName("read-more");
+  var moreText = document.getElementsByClassName("more");
+
+  if (moreText.style.display === "none") {
+    moreText.style.display = "inline";
+    read.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    moreText.style.display = "none";
+    read.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
