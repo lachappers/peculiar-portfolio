a11647c96536b02bfed03aa0389f74c9529ad15c
lachappers <116145040+lachappers@users.noreply.github.com>
2023-05-18 12:15:15 +0100

    portfolio cards formatted

--- src/index.js
+++ src/index.js
@@ -3,14 +3,4 @@ import "./css/style.css";
 import "./js/toggle-modal";
 import "./js/accordian";
 import "./js/nav-animation";
-
-function component() {
-  const element = document.createElement("div");
-
-  // Lodash, currently included via this script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
-
-  return element;
-}
-
-// document.body.appendChild(component());
+import "./js/clickable-card";
