81b0d62fc8fc2d7c6a43eab7550b20847b6ef423
lachappers <116145040+lachappers@users.noreply.github.com>
2023-07-04 16:59:20 +0100

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import "./css/style.css";
-import "./js/toggle-modal";
-import "./js/accordian";
-import "./js/nav-animation";
-// import "./js/clickable-card";
-import "./js/tooltip";
-import "./js/email";
-import "./js/scroll-to-top";
--- src/js/email.js
+++
@@ -1,7 +0,0 @@
-const encodedEmail = "aW5mb0BwZWN1bGlhcmR5bmFtaWNzLmNvLnVr";
-
-const form = document.getElementById("email-link");
-form.setAttribute(
-  "href",
-  "mailto:".concat(atob(encodedEmail)).concat("?subject=Portfolio Query")
-);
