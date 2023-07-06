23ca55e875dccfa7b7930fe0590b22e9d5212952
lachappers <116145040+lachappers@users.noreply.github.com>
2023-07-06 16:11:20 +0100

    resolved webpack config

---
+++ webpack.common.js
@@ -0,0 +1,71 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.js",
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Lucy Chaplin",
+      template: "./src/index.html",
+      inject: "body",
+    }),
+  ],
+  output: {
+    filename: "[name].[contenthash].js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+  },
+  stats: { children: true },
+  module: {
+    rules: [
+      {
+        test: /\.html$/i,
+        loader: "html-loader",
+      },
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif|svg|webp)$/,
+        type: "asset/resource",
+        generator: {
+          filename: "img/[name]-[hash][ext]",
+        },
+      },
+      {
+        test: /\.(png|jpg)$/i,
+        type: "asset",
+        parser: {
+          dataUrlCondition: {
+            maxSize: 10 * 1024, // Inline images under 10KB
+          },
+        },
+        generator: {
+          filename: "img/[name][hash][ext]",
+        },
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: "babel-loader",
+          },
+        ],
+      },
+      {
+        test: /\.(woff(2)?|ttf|eot)(\?v=\d+\.\d+\.\d+)?$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name].[ext]",
+              outputPath: "fonts/",
+            },
+          },
+        ],
+      },
+    ],
+  },
+};
